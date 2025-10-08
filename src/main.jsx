c586d857ed0ad782300552df9500c04d74e76ce2
Apon Talukder <apontalukder212@gmail.com>
2025-10-08 19:47:34 +0600

    All Files are update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppContexts from "./contexts/AppContexts.jsx";
+import AppRouter from "./routers/AppRouter.jsx";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <AppContexts>
+      <AppRouter />
+    </AppContexts>
+  </StrictMode>
+);
